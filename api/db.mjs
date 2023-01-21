3b6bddf8f7bc90f34492a76d73a32c847b9fc077
AlfredoSequeida <alfredosequeida@outlook.com>
2023-01-21 01:36:08 -0800

    removed usued vars and unnecessary try/catch wrapper

--- api/db.mjs
+++ api/db.mjs
@@ -1,25 +1,17 @@
 import { getDB } from "../database.mjs";
 
-const insertOne = async (insertData, res) => {
-    try {
-        const db = await getDB();
-        const testCollection = db.collection("test");
-        const response = await testCollection.insertOne(insertData);
-        return response.insertedId;
-    } catch (e) {
-        throw e;
-    }
+const insertOne = async (insertData) => {
+    const db = await getDB();
+    const testCollection = db.collection("test");
+    const response = await testCollection.insertOne(insertData);
+    return response.insertedId;
 };
 
-const find = async (res) => {
-    try {
-        const db = await getDB();
-        const testCollection = db.collection("test");
-        const cursor = testCollection.find();
-        return await cursor.toArray();
-    } catch (e) {
-        throw e;
-    }
+const find = async () => {
+    const db = await getDB();
+    const testCollection = db.collection("test");
+    const cursor = testCollection.find();
+    return await cursor.toArray();
 };
 
 export { insertOne, find };
